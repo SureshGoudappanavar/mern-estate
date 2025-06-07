604ecbdb72598ec573f3aa82f3c16529b50a80c3
Suresh Goudappanavar <sureshpg003@gmail.com>
2025-06-07 21:54:51 +0530

    create a test API

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -13,4 +13,7 @@ const app=express();
 
 app.listen(3000,()=>{
     console.log('Server is Running on 3000')
-})
+});
+
+
+app.use('/api/user',userRouter);
